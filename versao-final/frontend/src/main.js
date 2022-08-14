f5e051674f2bff2d80a7359f51df0403a5bcd653
Lucas Matos <lucas.suporte.tec@outlook.com>
2022-08-14 18:12:09 -0400

    add administration system

--- versao-final/frontend/src/main.js
+++ versao-final/frontend/src/main.js
@@ -1,20 +1,20 @@
-import 'font-awesome/css/font-awesome.css'
-import Vue from 'vue'
+import "font-awesome/css/font-awesome.css";
+import Vue from "vue";
 
-import App from './App'
+import App from "./App";
 
-import './config/bootstrap'
-import './config/msgs'
-import './config/axios'
-import './config/mq'
+import "./config/bootstrap";
+import "./config/msgs";
+import "./config/axios";
+import "./config/mq";
 
-import store from './config/store'
-import router from './config/router'
+import store from "./config/store";
+import router from "./config/router";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   store,
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
--- versao-inicial/frontend/src/main.js
+++ versao-inicial/frontend/src/main.js
@@ -4,13 +4,13 @@ import Vue from "vue";
 import App from "./App";
 
 import "./config/bootstrap";
+
 import store from "./config/store";
 import router from "./config/router";
 
 Vue.config.productionTip = false;
 
 //TEMPORARIO!
-
 require("axios").defaults.headers.common["Authorization"] =
   "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6OSwibmFtZSI6Imx1Y2FzIG1hdG9zIiwiZW1haWwiOiJsdWNhcy5tYXRvcy5pcHJAZ21haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTY2MDQ4MjUzNCwiZXhwIjoxNjYwNzQxNzM0fQ.0s5DmyFAJ_k5zCAyrLAeqtXuwR_BDkxnUkjuSXFYSwU";
 
